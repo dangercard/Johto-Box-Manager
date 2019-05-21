539ad39094677f35606dad4be834d45aee198844
Alejandro Deloach <alex.deloach9@gmail.com>
2019-05-20 23:00:25 -0400

    Remade program structure.

--- logic.js
+++ logic.js
@@ -4,53 +4,34 @@ mongoose.Promise = global.Promise;
 
 const db = mongoose.connect('mongodb://localhost:27017/Johto-Box-Manager',{ useNewUrlParser: true })
 
-function toLower(l){
-    return l.toLowerCase();
-}
-
-const PokemonSchema = mongoose.Schema({
-    species: {type: String, set: toLower},
-    type1: {type: String, set: toLower},
-    type2: {type: String, set: toLower}
-    // level: {type: Number, min:2 , max:100}
-});
-
-const Pokemon = mongoose.model('Pokemon', PokemonSchema);
-
-const BoxSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    availableSpace: {type:Number, min: 0, max:20},
-
-});
-
-const Box = mongoose.model('Box', BoxSchema);
-
 
 const BoxPokemonSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    box:{
-        type: mongoose.Schema.Types.ObjectId,   
-        ref: 'Box'
-    },
+    box:{type: Number, min:1, max:14},
     pokemon:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pokemon'
-    }
+             dexNum:{type: Number, min:1,max:251},
+             species:{type: String},
+             type1:{type: String},
+             type2:{type:String}
+            },
+
+    nickname:{type:String},
+    level:{type:Number, min:2,max:100}
 });
 
 const BoxPokemon = mongoose.model('BoxPokemon', BoxPokemonSchema);
 
-const addPokemon = (pokemon) => {
-    Pokemon.create(pokemon,(err)=> {
+const addBoxPokemon = (pokemon) => {
+    BoxPokemon.create(pokemon,(err)=> {
         assert.equal(null,err);
         console.info('New Pokemon Added.');
         mongoose.disconnect();
     });
 };
 
-const getPokemon = (name) => {
+const getBoxPokemon = (name) => {
     const search = new RegExp(name, 'i');
-    Pokemon.find({$or: [{species: search }]})
+    Pokemon.find({$or: [{species: search }, {type1: search}, {type2: search}]})
     .exec((err, pokemon) => {
       assert.equal(null, err);
       console.info(pokemon);
@@ -59,4 +40,4 @@ const getPokemon = (name) => {
     });
 }
 
-module.exports = {addPokemon,getPokemon};
+module.exports = {addBoxPokemon,getBoxPokemon};
--- pokemon.js
+++ pokemon.js
@@ -1,30 +1,82 @@
-const program = require('commander');
+// Main application file.
 
-const {addPokemon, getPokemon} = require('./logic');
+// Import all schema methods from the logic.js file.
+const {addBoxPokemon, getBoxPokemon} = require('./logic');
 
-program
-.version('1.0.0')
-.description('cli for managing your generation 2 PC boxes.');
+// Import prompts module.
+const prompts = require('./lib/prompts');
 
-program
-.command('addPokemon <species> <type1> [type2]')
-.alias('a')
-.description('add pokemon')
-.action((species, type1, type2) => {
-    if(type2 == undefined)
-    {
-        addPokemon({species,type1});
-    }
+// Function  choosePokemon:
+//      Prompts the selection of a pokemon 
+//      given the pokedex(array of all Pokemon objects). 
+async function choosePokemon(pokedex){
+    const chosenPokemon = await prompts({
+        // Prompt message:
+        message: "Choose the Pokemon you want to add.",
+        // Array of choices 
+        choices:pokedex
+    });
 
-    else{
-        addPokemon({species,type1,type2});
-    }
-});
+    // Return selected Pokemon object.
+    return chosenPokemon.value; 
+}
 
-program
-.command('getPokemon <name>')
-.alias('s')
-.description('Search for pokemon')
-.action(name => getPokemon(name));
 
-program.parse(process.argv);
+// Function chooseBox:
+//      Prompts the selection of a box
+//      given the list of Box numbers.
+async function chooseBox(boxes){
+    const chosenBox = await prompts({
+        // Prompt message.
+        message:"Choose the Box where you want to add the Pokemon.",
+        // Array of choices.
+        choices:boxes
+    });
+   
+    // Return selected Box.
+    return chosenBox.value;
+}
+
+// Main menu function
+async function addPokemon(){
+    let pokedex = [
+        {title:"Chikorita", value:{dexNum: 1,species:"Chikorita",type1:"Grass"}},
+        {title:"Bayleaf", value:{dexNum: 2,species:"Bayleaf",type1:"Grass"}}
+    ];
+
+    let boxes = [
+        {title:"Box1",value:1},
+        {title:"Box2",value:2}
+    ];
+    let chosenBox = await chooseBox(boxes); 
+    let chosenNewPokemon = await choosePokemon(pokedex);
+    console.info(`${chosenNewPokemon.species} added to Box${chosenBox}.`);
+}
+
+async function chooseOption(options){
+    const chosenOption = await prompts({
+        // Prompt message.
+        message:"Please select an option:",
+        // Array of choices.
+        choices:options
+    });
+   
+    // Return selected Box.
+    return chosenOption.value;
+}
+
+async function main(){
+    let options = [ 
+        {title:"1.  Add Pokemon", value: addPokemon},
+        {title:"2.  Find Pokemon", value:"soon"}
+    ];
+
+    let chosenOption = await chooseOption(options);
+    chosenOption();
+    
+    
+}
+
+
+
+main();
